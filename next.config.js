40f3444948e0cf4d9e32b32580c3c0dc5a86d222
unknown <tizen48@gmail.com>
2023-08-08 13:43:07 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: 'https://tizenmile.github.io/event_planner/',
   };
 
 module.exports = nextConfig
