1bb0db50ddd18ffefddd0de1ba897d3e55ae3320
unknown <tizen48@gmail.com>
2023-08-08 14:35:11 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "/event_planner/",
-  basePath: "/event_planner",
 };
 
 module.exports = nextConfig;
